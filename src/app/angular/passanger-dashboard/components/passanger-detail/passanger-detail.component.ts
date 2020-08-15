fb44280fbd719d6e9fb87ed04f8caab57d3cac77
christiankatka <christiankatka1996@outlook.com>
2020-08-15 12:42:44 +0300

    event emitting

--- src/app/angular/passanger-dashboard/components/passanger-detail/passanger-detail.component.ts
+++ src/app/angular/passanger-dashboard/components/passanger-detail/passanger-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Passanger } from 'src/app/angular/models/passanger.model';
 
 @Component({
@@ -11,9 +11,37 @@ export class PassangerDetailComponent implements OnInit {
   @Input()
   detail: Passanger;
 
+  // pass data from child to parent
+  @Output()
+  edit: EventEmitter<any> = new EventEmitter();
+  @Output()
+  remove: EventEmitter<any> = new EventEmitter();
+
+  // wether user is editing text or not
+  editing: boolean = false;
+
   constructor() {}
 
   ngOnInit() {
-      console.log('detail component:', this.detail);
+    // console.log('detail component:', this.detail);
+  }
+
+  onNameChange(value: string) {
+    console.log('detail component on namechange: ', value);
+    // saves changed value but only in this component
+    this.detail.fullname = value;
+  }
+
+  toggleEdit() {
+    if(this.editing) {
+      this.edit.emit(this.detail);
+    }
+    // toggle boolean value
+    this.editing = !this.editing;
+  }
+
+  // emits event to container passa passanger as parameter
+  onRemove() {
+    this.remove.emit(this.detail);
   }
 }
--- src/app/angular/passanger-dashboard/containers/passanger-dashboard/passanger-dashboard.container.ts
+++ src/app/angular/passanger-dashboard/containers/passanger-dashboard/passanger-dashboard.container.ts
@@ -39,6 +39,35 @@ export class PassangerDashboardContainer implements OnInit {
         checkedIn: true,
         checkInDate: null,
       },
-    ]; 
+    ];
+  }
+
+  // changes fullname of the passanger
+  handleEdit(event: Passanger) {
+    // console.log('container, handleEdit(): ',  event);
+    this.passangers = this.passangers.map((passanger: Passanger) => {
+      if (passanger.id === event.id) {
+        // päivitetään koko objecti eli korvataan passanger objecti uudella sisääntulleella event objektilla jolloin nimi muuttuu
+        passanger = Object.assign(passanger, event);
+        //  console.log('container if lauseen sisässä: ', passanger);
+      }
+      console.log(passanger);
+      // returns modified version of the passangers
+      return passanger;
+    });
+  }
+
+  // removes choosen passanger from the array
+  handleRemove(event: Passanger) {
+    // console.log('container, handleremove: ', event);
+
+    this.passangers = this.passangers.filter((passanger: Passanger) => {
+      // palauttaa kaikki passangerit millä ei ole sama iidee kun parametris tarjotulla.
+      return passanger.id != event.id;
+      // eli poistaa sen passangerin minkä id mätchää parametrina tarjottuun iideeseen
+    });
+
+    //lyhyempi tapa mut ei kannata käyttää koska silloin ei voi määrittää tyyppiä
+    // this.passangers = this.passangers.filter(passanger => passanger.id != event.id)
   }
 }
